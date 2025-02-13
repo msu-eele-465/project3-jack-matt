28b62a62d161fc4e117c8abe5b9a0ca776609655
M. Aiden Phillips <mphillips8687@gmail.com>
2025-02-13 15:09:55 -0700

    updating pseudocode

--- app/keypad.c
+++ app/keypad.c
@@ -10,12 +10,34 @@ void keypad_init() {
 void keypad_update(int input[8]) {
     /*
         Intended behavior:
+        * if locked, log
+        * if unlocked
+          - lock if input 'D' then return
+          - set pattern then return
+    */
+
+}
+
+void keypad_log_input(int input[8]) {
+    /*
+        Intended behavior:
         * Log input
           - or clear if D
         * If last of 4, check against unlock code(s)
           - unlock if incorrect
           - clear log if not
         * once unlocked, input drives LED patterns
+    */
+
+}
+
+void keypad_set_pattern(int input[8]) {
+    /*
+        Intended behavior:
+        * case 0 -> set pattern 0
+        * case 1 -> set pattern 1
+        * case 2 -> set pattern 2
+        * etc.
     */
 
 }
