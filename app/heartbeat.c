6a4288281140e7fb25f4b897ff3c33b5a7f4685b
M. Aiden Phillips <mphillips8687@gmail.com>
2025-02-13 13:54:23 -0700

    re-added pseudo-code in heartbeatLED, lost from migration

--- app/heartbeat.c
+++ app/heartbeat.c
@@ -31,6 +31,7 @@ void heartbeat_init(void) {
 }
 
 void heartbeat_run(void) {
+  // flip the P6.6 bit to alternate the LED on/off
   P6OUT ^= BIT6;
   return;
 }
--- app/heartbeat.h
+++ app/heartbeat.h
@@ -4,8 +4,10 @@
 #include <stdint.h>
 #include <stddef.h>
 
+// a function to initialize the P6.6 and TIMER1_B0_VECTOR isr
 void heartbeat_init(void);
 
+// a function to alternate the P6.6 LED
 void heartbeat_run(void);
 
 #endif // HEARTBEAT_H
