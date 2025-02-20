ce34868e109a025fdde3fe2619e1eef656b6b201
Matt Phillips <mphillips8687@gmail.com>
2025-02-20 11:02:14 -0700

    fixing pin assignments and comments associated

--- app/keypad.h
+++ app/keypad.h
@@ -16,10 +16,10 @@
 #define ROW_PINS (ROW1_PIN | ROW2_PIN | ROW3_PIN | ROW4_PIN)
 #define ROW_PORT P4OUT
 
-#define COL1_PIN BIT0 // P4.0
-#define COL2_PIN BIT1 // P4.1
-#define COL3_PIN BIT2 // P4.2
-#define COL4_PIN BIT3 // P4.3
+#define COL1_PIN BIT0 // P6.0
+#define COL2_PIN BIT1 // P6.1
+#define COL3_PIN BIT2 // P6.2
+#define COL4_PIN BIT3 // P6.3
 #define COL_PINS (COL1_PIN | COL2_PIN | COL3_PIN | COL4_PIN)
 #define COL_PORT P6OUT
 #define COL_IN P6IN
