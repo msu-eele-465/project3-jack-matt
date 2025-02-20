8f0df3020963401fc9c974fcf56856eb712caf8e
Matt Phillips <mphillips8687@gmail.com>
2025-02-20 11:34:02 -0700

    adding rgb led init and base operation

--- app/keypad.c
+++ app/keypad.c
@@ -12,11 +12,31 @@ static const char unlock_code[4] = {'1', '2', '3', '4'};
 static char entered_code[4];
 static unsigned char code_index = 0;
 static bool is_unlocked = false;
+static system_state_t current_state = SYSTEM_LOCKED;
 
 void keypad_init(void) {
   // Configure LED pin as output
   P1DIR |= LED_PIN;
   LED_PORT &= ~LED_PIN; // LED off initially
+
+  // Configure RGB LED pins for PWM
+  P1DIR |= RGB_RED_PIN | RGB_GREEN_PIN | RGB_BLUE_PIN; // P2.6, P2.7
+
+  P2SEL0 |= RGB_RED_PIN | RGB_GREEN_PIN; // Select PWM function
+  P2SEL1 &= ~(RGB_RED_PIN | RGB_GREEN_PIN);
+  P3SEL0 |= RGB_BLUE_PIN;
+  P3SEL1 &= ~RGB_BLUE_PIN;
+
+  // Configure Timer B1 for PWM - only 2 channels available
+  TB1CCR0 = 255;                // PWM period (8-bit resolution)
+  TB1CCTL1 = OUTMOD_7;         // Red on TB1.1
+  TB1CCTL2 = OUTMOD_7;         // Green on TB1.2
+  TB1CTL = TBSSEL__SMCLK | MC__UP | TBCLR;  // SMCLK, up mode, clear counter
+
+  // We'll need to use a different timer for blue, like Timer B0
+  TB0CCR0 = 255;
+  TB0CCTL1 = OUTMOD_7;         // Blue on TB0.1
+  TB0CTL = TBSSEL__SMCLK | MC__UP | TBCLR;
 
   // Configure row pins as outputs
   P4DIR |= ROW_PINS;
@@ -26,6 +46,14 @@ void keypad_init(void) {
   P6DIR &= ~COL_PINS;
   P6REN |= COL_PINS; // Enable pull-up/down resistors
   P6OUT |= COL_PINS; // Set pull-up
+
+  rgb_set_color(196, 62, 29);
+}
+
+void rgb_set_color(unsigned char red, unsigned char green, unsigned char blue) {
+  TB1CCR1 = red;   // Red
+  TB1CCR2 = green; // Green
+  TB0CCR1 = blue;  // Blue
 }
 
 char keypad_scan(void) {
@@ -87,6 +115,15 @@ bool keypad_check_unlock(void) {
   }
 
   is_unlocked = match;
+
+  if (match) {
+    current_state = SYSTEM_UNLOCKED;
+    rgb_set_color(29, 162, 196); // #1da2c4 blue
+  } else {
+    current_state = SYSTEM_LOCKED;
+    rgb_set_color(196, 62, 29); // #c43e1d red-orange
+  }
+
   return match;
 }
 
--- app/keypad.h
+++ app/keypad.h
@@ -1,25 +1,30 @@
-#ifndef KEYPAD_H_
-#define KEYPAD_H_
+#ifndef KEYPADH
+#define KEYPADH
 
 #include <msp430.h>
 #include <stdbool.h>
 
-// LED pin definition
-#define LED_PIN BIT0 // P1.1
+// Status LED pin definition
+#define LED_PIN BIT0      // P1.0
 #define LED_PORT P1OUT
 
-// Keypad pin definitions
-#define ROW1_PIN BIT4 // P4.4
-#define ROW2_PIN BIT5 // P4.5
-#define ROW3_PIN BIT6 // P4.6
-#define ROW4_PIN BIT7 // P4.7
+// RGB LED pins (using Timer_B PWM)
+#define RGB_RED_PIN   BIT7    // P1.7 - TB1.1
+#define RGB_GREEN_PIN BIT6    // P1.6 - TB1.2
+#define RGB_BLUE_PIN  BIT5    // P1.5 - TB1.3
+
+// Keypad pin definitions 
+#define ROW1_PIN BIT4     // P4.4
+#define ROW2_PIN BIT5     // P4.5
+#define ROW3_PIN BIT6     // P4.6
+#define ROW4_PIN BIT7     // P4.7
 #define ROW_PINS (ROW1_PIN | ROW2_PIN | ROW3_PIN | ROW4_PIN)
 #define ROW_PORT P4OUT
 
-#define COL1_PIN BIT0 // P6.0
-#define COL2_PIN BIT1 // P6.1
-#define COL3_PIN BIT2 // P6.2
-#define COL4_PIN BIT3 // P6.3
+#define COL1_PIN BIT0     // P6.0
+#define COL2_PIN BIT1     // P6.1
+#define COL3_PIN BIT2     // P6.2
+#define COL4_PIN BIT3     // P6.3
 #define COL_PINS (COL1_PIN | COL2_PIN | COL3_PIN | COL4_PIN)
 #define COL_PORT P6OUT
 #define COL_IN P6IN
@@ -29,5 +34,13 @@ void keypad_init(void);
 char keypad_scan(void);
 bool keypad_check_unlock(void);
 bool keypad_is_unlocked(void);
+void rgb_set_color(unsigned char red, unsigned char green, unsigned char blue);
 
-#endif /* KEYPAD_H_ */
+// System states
+typedef enum {
+    SYSTEM_LOCKED,
+    SYSTEM_UNLOCKING,
+    SYSTEM_UNLOCKED
+} system_state_t;
+
+#endif /* KEYPADH */
